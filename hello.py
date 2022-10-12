13d750974d601b9bea1de89f541c435b712883d5
OscarTeran <ohernandez@cbpi.com.mx>
2022-10-12 13:04:09 -0500

    2da revisión y comienzo en CodigoFacilito

--- hello.py
+++ hello.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello world</h1>'
+
+
+# Dynamic Routes
+@app.route('/user/Oscar')
+def user(Oscar):
+    return '<h1>Hello, {}</h1>'.format(Oscar)
+
