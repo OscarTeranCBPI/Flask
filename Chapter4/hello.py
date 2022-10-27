0af251a420329e03e3dd6ca251bcce545b7b2538
OscarTeran <oscarteran@kali-cbpi.KALI>
2022-10-27 17:05:56 -0500

    Completed Chapter 4

---
+++ Chapter4/hello.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html', current_time=datetime.utcnow())
+
+# With this command, formulario send again
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     name = None
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         name = form.name.data
+#         form.name.data = ''
+#     return render_template('index.html', form=form, name=name)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         session['name'] = form.name.data
+#         return redirect(url_for('index'))
+#     return render_template('index.html', form=form, name=session.get('name'))
+
+# Flashed messages
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+            form = form, name = session.get('name'))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
