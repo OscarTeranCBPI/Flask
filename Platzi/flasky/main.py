216fae6338a47cd1fd7f74ea5746346b0d3ff597
OscarTeran <oscarteran@kali-cbpi.KALI>
2022-11-11 09:09:40 -0600

    Comienzo del capítulo 9

---
+++ Platzi/flasky/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+# Nueva instancia de Flask
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+# Confiración de llaves para emcriptación
+app.config['SECRET_KEY'] = 'Super secreto'
+
+# Variable para iteraciones
+todos = ['Comprar café', 'Solicitud de compra', 'Productor']
+
+# wft
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+
+# Configuraciones de errores
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+# Creación de nueva ruta
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
+    return response
+
+
+# Creación de ruta a través de una función
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+    context = {
+        'user_ip':user_ip, 
+        'todos':todos,
+        'login_form':login_form,
+        'username':username
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
+
+
+    return render_template('hello.html', **context)
