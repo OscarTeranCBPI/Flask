4050b4991613c6c4c585b855365f527bfe4c313a
OscarTeran <oscarteran@kali-cbpi.KALI>
2022-11-14 10:44:07 -0600

    Capitulo 9. Problemas con la base de datos

--- Chapter9/flasky/app/__init__.py
+++ Chapter9/flasky/app/__init__.py
@@ -5,6 +5,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+# from .models import Permission
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -33,3 +34,10 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
+
+
+# @main.app_context_processor
+# def inject_permissions():
+#     return dict(Permission=Permission)
+
+
--- Chapter9/flasky/tests/test_user_model.py
+++ Chapter9/flasky/tests/test_user_model.py
@@ -1,7 +1,7 @@
 import unittest
 import time
 from app import create_app, db
-from app.models import User
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -58,3 +58,19 @@ class UserModelTestCase(unittest.TestCase):
         token = u.generate_confirmation_token(1)
         time.sleep(2)
         self.assertFalse(u.confirm(token))
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
